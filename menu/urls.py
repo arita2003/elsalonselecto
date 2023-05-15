51492b00aa082b7305e5d4252812201750e4514b
arita2003 <127121948+arita2003@users.noreply.github.com>
2023-05-15 12:04:48 -0400

    parte1

---
+++ menu/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import entorno, index
+
+urlpatterns = [
+    path('',index,name="index"),
+    path('entorno/',entorno,name="entorno"),
+]
