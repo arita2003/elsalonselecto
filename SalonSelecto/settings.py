13fc7c60d18faea1e81f398cb8cf136d2e5b36b8
enicxus <tineoanderson7@gmail.com>
2023-05-20 00:44:38 -0400

    porfinindex1.0

--- SalonSelecto/settings.py
+++ SalonSelecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,19 +75,19 @@ WSGI_APPLICATION = 'SalonSelecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl',
-        'USER': 'salonselecto',
-        'PASSWORD': 'salonselecto',
-        'TEST':{
-            'USER': 'default_test',
-            'TBLSPACE': 'default_test_tbls',
-            'TBLSPACE_TMP': 'default_test_tbls_tmp',
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.oracle',
+#         'NAME': '127.0.0.1:1521/orcl',
+#         'USER': 'salonselecto',
+#         'PASSWORD': 'salonselecto',
+#         'TEST':{
+#             'USER': 'default_test',
+#             'TBLSPACE': 'default_test_tbls',
+#             'TBLSPACE_TMP': 'default_test_tbls_tmp',
+#         },
+#     }
+# }
 
 
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- menu/models.py
+++ menu/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 #ejemplos despues hay que cambiarlos
-class nombre (models.model):
-    codigonombre=models.AutoField(primary_key=True) 
-    nombreraza=models.CharField(max_length=15)
+#class nombre (models.Model):
+#    codigonombre=models.AutoField(primary_key=True) 
+#   nombreraza=models.CharField(max_length=15)
 
-class Mascota(models.Model):
-    codigoChip = models.CharField(max_length=20, primary_key=True)
-    nombreMascota = models.CharField(max_length=25,verbose_name='Nombre de la mascota')
-    edadMascota = models.IntegerField()
-    foto = models.ImageField(upload_to="mascota")
-    raza = models.ForeignKey(Raza,on_delete=models.CASCADA)
+#class Mascota(models.Model):
+#    codigoChip = models.CharField(max_length=20, primary_key=True)
+#    nombreMascota = models.CharField(max_length=25,verbose_name='Nombre de la mascota')
+#    edadMascota = models.IntegerField()
+#    foto = models.ImageField(upload_to="mascota")
+#    raza = models.ForeignKey(Raza,on_delete=models.CASCADA)
