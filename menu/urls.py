700424899aff85e828a1503d03658170a78639be
enicxus <tineoanderson7@gmail.com>
2023-05-19 23:09:32 -0400

    nopuedomas:(1.0

--- menu/urls.py
+++ menu/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,incluide
 from .views import entorno, index
 
 urlpatterns = [
-    path('',index,name="index"),
-    path('entorno/',entorno,name="entorno"),
+    path('', index, name="index"),
+    path('entorno/', entorno, name="entorno"),
 ]
-if setings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
