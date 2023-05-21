e5c6fed640ca32b76efaeb53e692bae1c401a765
enicxus <tineoanderson7@gmail.com>
2023-05-21 13:56:30 -0400

    omg_

--- menu/urls.py
+++ menu/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import entorno, index, recuperacion, recuperacion2, recuperacion3
+from .views import  index 
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('recuperacion.thml', recuperacion, name="recuperacion"),
-    path('recuperacion2.html', recuperacion2, name="recuperacion2"),
-    path('recuperacion3.html', recuperacion3, name="recuperacion3"),
-    path('entorno.html', entorno, name="entorno")
-
-
+    path('', index, name="index")
 ]
