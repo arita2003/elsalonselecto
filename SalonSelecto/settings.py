fd896d127380e85e58531224721ae3f51b580620
enicxus <tineoanderson7@gmail.com>
2023-05-21 22:02:50 -0400

    kabscishfoihwoihf

--- SalonSelecto/settings.py
+++ SalonSelecto/settings.py
@@ -76,21 +76,26 @@ WSGI_APPLICATION = 'SalonSelecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.oracle',
-#         'NAME': '127.0.0.1:1521/orcl',
-#         'USER': 'salonselecto',
-#         'PASSWORD': 'salonselecto',
-#         'TEST':{
-#             'USER': 'default_test',
-#             'TBLSPACE': 'default_test_tbls',
-#             'TBLSPACE_TMP': 'default_test_tbls_tmp',
-#         },
-#     }
-# }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.oracle',
+#        'NAME': '127.0.0.1:1521/orcl',
+#        'USER': 'salonselecto',
+#        'PASSWORD': 'salonselecto',
+#        'TEST': {
+#                'USER': 'default_test',
+#                'TBLSPACE': 'default_test_tbls',
+#                'TBLSPACE_TMP': 'default_test_tbls_tmp',
+#        },
+#    }
+#}
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -137,4 +142,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
