77098dd46978fc4b83b2e1b8edbc144c35d03ab7
MatiasBSC <mati.sandoval@duocuc.cl>
2023-05-22 11:11:41 -0400

    aña

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pedido, Detalle, Comida, Categoria, Usuario, Pregunta, Rol, Direccion, Comuna, Region
 
 # Register your models here.
+
+admin.site.register(Pedido)
+admin.site.register(Detalle)
+admin.site.register(Comida)
+admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Pregunta)
+admin.site.register(Rol)
+admin.site.register(Direccion)
+admin.site.register(Comuna)
+admin.site.register(Region)
