c265cb5be77960931104852cf63d92a6fa8a5d0e
arita2003 <127121948+arita2003@users.noreply.github.com>
2023-06-09 11:55:33 -0400

    seguridad

--- rest_direccion/views.py
+++ rest_direccion/views.py
@@ -6,11 +6,32 @@ from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from menu.models import Direccion
 from .serializers import direccionSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from rest_framework.authtoken.models import token
 
 # Create your views here.
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
+
+def login (request):
+    data = JASONPaser().parase(request)
+
+    username = data['username']
+    check_password = data['password']
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response ("usuario invalido")
+    #validamos la contra
+    pass_valido =check_password(password,user.password)
+    if not pass_valido:
+        return Response ("password incorrecta")
+    #permitir o recuperar al token
+    token, created = Token.objects.get_or_create(user.user)
+    #print (token.key)
+    return Response(token.key)    
 
 def datos_direccion(request):
 
