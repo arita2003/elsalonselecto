0c0a7597e32ba7a46f191462057438a8912fa46b
BastianMunozF <ba.munozf@duocuc.cl>
2023-06-11 14:04:31 -0400

    Borrado de tabla categoria y boolean en tabla comida

--- menu/models.py
+++ menu/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-class Categoria(models.Model):
-    id_categoria = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    def __str__(self):
-        return self.nombre
-
 class Pregunta(models.Model):
     id_pregunta = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
@@ -31,7 +25,8 @@ class Comida(models.Model):
     ingredientes = models.TextField()
     foto = models.ImageField(upload_to='comida')
     precio = models.IntegerField(default=0)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    especial = models.BooleanField()
+    
     def __str__(self):
         return self.nombre
 
@@ -93,5 +88,3 @@ class Detalle(models.Model):
     
     def __str__(self) :
         return self.cantidad
-
-
